c7ce3f2978b99a9f3b862bd4e486362afdbb3f7b
Vlad Shtrikun <vlad.shtrikun@bndigital.co>
2025-04-16 19:03:47 +0300

    Add progress management for register form

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -13,8 +13,15 @@ import { useTranslation } from "react-i18next";
 import BackButton from "../shared/BackButton";
 import RegisterFirstStep from "./RegisterFirstStep";
 import RegisterSecondStep from "./RegisterSecondStep";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+
+const stepFields: Record<number, (keyof IRegisterFormValues)[]> = {
+  0: ["email", "firstName", "lastName", "password", "confirmPassword"],
+};
 
 const RegisterForm = () => {
+  const router = useRouter();
   const { t } = useTranslation();
   const form = useForm<IRegisterFormValues>({
     mode: "onChange",
@@ -23,10 +30,33 @@ const RegisterForm = () => {
       subscribe: false,
     },
   });
+  const { trigger, handleSubmit, formState } = form;
+  const [registrationProgress, setRegistrationProgress] = useState(0);
 
-  const onSubmit = async (data: any) => {
-    console.log("Register data", data);
+  const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const currentStepFields = stepFields[registrationProgress];
+
+    const isValid = await trigger(currentStepFields);
+
+    if (!isValid) return;
+
+    if (registrationProgress === 0) {
+      setRegistrationProgress(1);
+    } else {
+      handleSubmit((data) => {
+        console.log("Form data", data);
+      })();
+    }
   };
+
+  const handleBack = () => {
+    registrationProgress === 0
+      ? router.push("/admin/login")
+      : setRegistrationProgress(0);
+  };
+
   return (
     <Box
       sx={{
@@ -51,8 +81,8 @@ const RegisterForm = () => {
         Register
       </Typography>
       <FormProvider {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <BackButton handleBack={() => console.log("back")} />
+        <form onSubmit={onSubmit}>
+          <BackButton handleBack={handleBack} />
           <Box
             sx={{
               display: "flex",
@@ -62,8 +92,11 @@ const RegisterForm = () => {
               mt: "16px",
             }}
           >
-            <RegisterFirstStep />
-            <RegisterSecondStep />
+            {registrationProgress === 0 ? (
+              <RegisterFirstStep />
+            ) : (
+              <RegisterSecondStep />
+            )}
           </Box>
 
           <Box sx={{ margin: "16px 0px" }}>
