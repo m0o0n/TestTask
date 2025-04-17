d2cde126a65402a2ecdd57733067ab9018a58936
Vlad Shtrikun <vlad.shtrikun@bndigital.co>
2025-04-17 21:46:30 +0300

    Add styles

--- theme/theme.ts
+++ theme/theme.ts
@@ -35,6 +35,10 @@ const theme = createTheme({
           lineHeight: 1.75,
           padding: "6px 8px",
           color: theme.palette.primary.main,
+          "&:hover": {
+            transition:
+              "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1), box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1), border-color 250ms cubic-bezier(0.4, 0, 0.2, 1), color 250ms cubic-bezier(0.4, 0, 0.2, 1)",
+          },
         }),
 
         contained: ({ theme }) => ({
@@ -45,6 +49,44 @@ const theme = createTheme({
           "&:hover": {
             boxShadow:
               "rgba(102, 112, 133, 0.2) 0px 2px 4px -1px, rgba(102, 112, 133, 0.14) 0px 4px 5px 0px, rgba(102, 112, 133, 0.12) 0px 1px 10px 0px",
+          },
+        }),
+      },
+    },
+
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          borderRadius: "8px",
+          background: "transparent",
+
+          "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+            borderColor: theme.palette.primary.main,
+          },
+
+          "&.Mui-error .MuiOutlinedInput-notchedOutline": {
+            borderColor: theme.palette.error.main,
+          },
+        }),
+
+        input: {
+          padding: "12px 14px",
+        },
+      },
+    },
+
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+            top: "-3px",
+            left: "-4px",
+            padding: "0px 4px",
+
+          "&.Mui-focused": {
+            color: theme.palette.primary.main,
+          },
+          "&.Mui-error": {
+            color: theme.palette.error.main,
           },
         }),
       },
